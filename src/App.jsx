b73a24944a4d3c987edc24fd978370bfb59f1010
ssk-webdev <wizard070904@gmail.com>
2025-11-20 11:28:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CliqTrix 2026!</h1>
-      <button className="fancy-btn">Original Button</button>
+      <header className="App-header">
+        <h1>Hello CliqTrix 2026!</h1>
+        <button className="fancy-btn">Original Blue Button</button>
+        <p>Waiting for designer review...</p>
+      </header>
     </div>
   );
 }
